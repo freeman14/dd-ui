dd66b285d49c9f9fcb34bd1f627335496e942d01
freeman14 <dubovetzky@me.com>
2016-10-06 13:19:47 +0300

    Remove js file for stories

--- src/stories/index.js
+++
@@ -1,6 +0,0 @@
-import 'ui-storybook/sb';
-import 'ui-storybook/stories';
-
-let overview = sb.section('Demo page');
-overview.story('Buttons')
-  .add('Button with text', '<button ng-bind="vm.text"></button>', { text: 'Demo button' });
