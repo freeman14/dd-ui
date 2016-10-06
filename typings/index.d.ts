e48ddcf8cf874d28e940713eca5d4ab5aee31866
freeman14 <dubovetzky@me.com>
2016-10-06 12:33:53 +0300

    Resolve merge conflict

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 /// <reference path="globals/angular-mocks/index.d.ts" />
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/source-map/index.d.ts" />
 /// <reference path="globals/uglify-js/index.d.ts" />
-=======
-/// <reference path="globals/angular/index.d.ts" />
->>>>>>> b2ac93bb9fdddd137062e284167e69bac10f6061
