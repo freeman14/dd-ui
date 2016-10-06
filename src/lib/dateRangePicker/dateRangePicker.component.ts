2b1c5bdd7b8e1d4ea4592b2cff07bb9fb35a236a
pkurochka <p.kurochka@svitla.com>
2016-10-06 18:47:45 +0300

    minor fixes

--- src/lib/dateRangePicker/dateRangePicker.component.ts
+++ src/lib/dateRangePicker/dateRangePicker.component.ts
@@ -1,22 +1,14 @@
 import { DateRangePickerController} from './dateRangePicker.controller';
 
-export class DateRangePicker implements ng.IComponentOptions {
+export const DateRangePickerComponent: ng.IComponentOptions = {
+  controller: DateRangePickerController,
+  controllerAs: 'vm',
+  template: require('./dateRangePicker.partial.html'),
+  transclude: true,
+  bindings: {
+    title: '@titleText',
+    onDateRangeChange: '&'
+  }
+};
 
-    public bindings: { [index: string]: string; };
-    public controller: Function;
-    public templateUrl: string;
-    public controllerAs: string;
-    public transclude: boolean;
 
-    constructor() {
-        this.controller = DateRangePickerController;
-        this.transclude = true;
-        this.controllerAs = 'vm';
-        this.templateUrl = './app/blocks/dateRangePicker/dateRangePicker.partial.html';
-        this.bindings = {
-            title: '@titleText',
-            onDateRangeChange: '&'
-        };
-    }
-
-}
--- src/lib/dateRangePicker/dateRangePicker.module.ts
+++ src/lib/dateRangePicker/dateRangePicker.module.ts
@@ -1,10 +1,13 @@
 import * as angular from 'angular';
+import * as moment from 'moment';
+
 import 'ob-daterangepicker/dist/scripts/ob-daterangepicker';
-import { DateRangePicker } from './dateRangePicker.component';
+import { DateRangePickerComponent } from './dateRangePicker.component';
 
 import './dateRangePicker.less';
 
 const module: ng.IModule = angular.module('ep.dateRangePicker', ['obDateRangePicker']);
-module.component('epDateRangePicker', new DateRangePicker);
+module.component('epDateRangePicker', DateRangePickerComponent);
+module.constant('moment', moment); // todo: remove dirty hack
 
 export default module.name;
