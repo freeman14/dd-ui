8df3a1192e8cdc70a0dd05272c2fd442f4eabeb1
freeman14 <dubovetzky@me.com>
2016-10-13 15:54:47 +0300

    Added DP demo with footer

--- src/stories/dateRangePicker/index.ts
+++ src/stories/dateRangePicker/index.ts
@@ -3,18 +3,37 @@ import * as moment from 'moment';
 let overview: stotybook.ISection = sb.section('UI components');
 overview.story('Date range picker')
   .add('Simple date range picker', '<ep-date-range-picker title-text="{{vm.title}}" range="vm.range" event-range="vm.eventRange"></ep-date-range-picker>',
-    {
-      title: 'Date picker',
-      date: {
-        start: moment().format('MM/DD/YYYY'),
-        end: moment().format('MM/DD/YYYY')
-      },
-      range: {
-        start: new Date(),
-        end: new Date()
-      },
-      eventRange: {
-        start: new Date(),
-        end: new Date()
-      }
-    });
+  {
+    title: 'Date picker',
+    date: {
+      start: moment().format('MM/DD/YYYY'),
+      end: moment().format('MM/DD/YYYY')
+    },
+    range: {
+      start: new Date(),
+      end: new Date()
+    },
+    eventRange: {
+      start: new Date(),
+      end: new Date()
+    }
+  })
+  .add('Date picker with custom footer',
+  `<ep-date-range-picker title-text="{{vm.title}}" range="vm.range" event-range="vm.eventRange">
+    <div>This is custom footer</div>
+  </ep-date-range-picker>`,
+  {
+    title: 'Date picker',
+    date: {
+      start: moment().format('MM/DD/YYYY'),
+      end: moment().format('MM/DD/YYYY')
+    },
+    range: {
+      start: new Date(),
+      end: new Date()
+    },
+    eventRange: {
+      start: new Date(),
+      end: new Date()
+    }
+  });
