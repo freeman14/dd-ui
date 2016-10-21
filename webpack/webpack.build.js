a4d45b0443297b79056a0f9be83919fabfce8d74
freeman14 <dubovetzky@me.com>
2016-10-21 17:07:23 +0300

    Update build process

--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -1,13 +1,10 @@
 var loaders = require("./loaders");
 var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 var NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 module.exports = {
   entry: {
-    'dd-ui': ['./src/lib/index.ts'],
-    app: './src/index.ts',
-    stories: './src/stories'
+    'dd-ui': ['./src/index.ts']
   },
   output: {
     libraryTarget: 'umd',
@@ -29,14 +26,10 @@ module.exports = {
   plugins: [
     new NgAnnotatePlugin({
       add: true
-    }),
-    new CopyWebpackPlugin([
-      { from: './src/preview.html', to: 'preview.html' },
-      { from: './src/index.html', to: 'index.html' }
-    ])
+    })
   ],
   module: {
     loaders: loaders,
-    noParse: [ "angular" ]
+    noParse: ["angular"]
   }
 };
