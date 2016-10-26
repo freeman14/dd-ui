7eeba4c2bcd5b77904da23a1901eb0e531188b9f
freeman14 <dubovetzky@me.com>
2016-10-26 13:07:29 +0300

    Added demo card styles

--- sb/stories/timestamp/index.ts
+++ sb/stories/timestamp/index.ts
@@ -1,13 +1,13 @@
 
 let overview: stotybook.ISection = sb.section('UI components');
 overview.story('Timestamps')
-  .add('Timestamp', '<timestamp date="vm.date" timezone="vm.tz" format="vm.format"></timestamp>',
+  .add('Timestamp', '<timestamp date="vm.date" timezone="vm.tz" format="vm.format" class="demo__card"></timestamp>',
     {
       date: new Date(),
       tz: 'America/Los_Angeles',
       format: 'ddd, MMM D, YYYY'
     })
-  .add('Timestamp Range', '<timestamps-range start="vm.start" end="vm.end" tz="vm.tz" format="vm.format"></timestamps-range>',
+  .add('Timestamp Range', '<timestamps-range start="vm.start" end="vm.end" tz="vm.tz" format="vm.format" class="demo__card"></timestamps-range>',
     {
       start: '2016-10-21T08:00:00.002Z',
       end: '2016-10-26T09:45:00.002Z',
--- sb/stories/toggleButtons/index.ts
+++ sb/stories/toggleButtons/index.ts
@@ -1,6 +1,6 @@
 let toggleButtons: stotybook.ISection = sb.section('UI components');
 toggleButtons.story('Toggle buttons')
-  .add('Simple toggle group', `<div class="btn-group">
+  .add('Simple toggle group', `<div class="btn-group demo__card">
         <button class="btn btn-primary" ng-model="vm.radioModel" uib-btn-radio="'Left'">Left</button>
         <button class="btn btn-primary" ng-model="vm.radioModel" uib-btn-radio="'Middle'">Middle</button>
         <button class="btn btn-primary" ng-model="vm.radioModel" uib-btn-radio="'Right'">Right</button>
