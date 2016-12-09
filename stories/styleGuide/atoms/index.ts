313230d4bd86b5b96d7b76a846d2c6e783f8d4ad
freeman14 <dubovetzky@me.com>
2016-12-09 13:00:21 +0200

    Inputs

--- stories/styleGuide/atoms/index.ts
+++ stories/styleGuide/atoms/index.ts
@@ -2,5 +2,6 @@ let atoms: stotybook.ISection = sb.section('Atoms');
 
 atoms.story('Atoms')
     .add('Buttons', require('./buttons.html'))
-    .add('Typography', require('./typography.html'));
+    .add('Typography', require('./typography.html'))
+    .add('Inputs', require('./inputs.html'));
 
