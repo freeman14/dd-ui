82c531242112cbd0a85fec11f91bc2474a9d3091
pkurochka <p.kurochka@svitla.com>
2016-12-15 13:27:27 +0200

    - image placeholder

--- stories/styleGuide/atoms/index.ts
+++ stories/styleGuide/atoms/index.ts
@@ -3,5 +3,6 @@ let atoms: stotybook.ISection = sb.section('Atoms');
 atoms.story('Atoms')
     .add('Buttons', require('./buttons.html'))
     .add('Typography', require('./typography.html'))
-    .add('Inputs', require('./inputs.html'));
+    .add('Inputs', require('./inputs.html'))
+    .add('Placeholders', require('./placeholders.html'));
 
