857aba15838a3d32f7097d0245d667d270fdbb3a
freeman14 <dubovetzky@me.com>
2016-12-23 14:46:42 +0200

    Added checkbox

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,9 +1,10 @@
 import './directives.less';
 import { InputLimit } from './input/limit';
-
+import checkbox from './checkbox/checkbox.directive';
 
 const module: ng.IModule = angular.module('directives', []);
 
 module.directive('textLimit', InputLimit.instance());
+module.directive('checkbox', checkbox);
 
 export default module.name;
