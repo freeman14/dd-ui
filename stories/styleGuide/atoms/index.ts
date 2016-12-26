17405126a703944be6ee3d8ff29630990edf9689
nick-bogdanov <kol9dyn4ek@gmail.com>
2016-12-26 19:15:28 +0200

    radio styles (#19)
    * radio styles
    
    * some clearup
    
    * added radio button as directive
    
    * added radio button as directive

--- stories/styleGuide/atoms/index.ts
+++ stories/styleGuide/atoms/index.ts
@@ -3,7 +3,11 @@ let atoms: stotybook.ISection = sb.section('Atoms');
 atoms.story('Atoms')
     .add('Buttons', require('./buttons.html'))
     .add('Typography', require('./typography.html'))
-    .add('Inputs', require('./inputs.html'))
+    .add('Inputs', require('./inputs.html'), {
+      radioButtons: {
+        value: 'option1'
+      }
+    })
     .add('Loading', require('./loading.html'))
     .add('Placeholders', require('./placeholders.html'));
 
