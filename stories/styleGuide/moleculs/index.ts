92be38c6ff8701a42c419fba31091d4f78fc9103
Dmitriy <dubovetzky@me.com>
2016-12-26 18:15:55 +0200

    Added basic select. (#22)

--- stories/styleGuide/moleculs/index.ts
+++ stories/styleGuide/moleculs/index.ts
@@ -1,4 +1,28 @@
 let moleculs: stotybook.ISection = sb.section('Moleculs');
 
 moleculs.story('Moleculs')
-  .add('Image uploader', require('./uploaders.html'));
+  .add('Image uploader', require('./uploaders.html'))
+  .add('Select', require('./select.html'), {
+    country: {},
+    countries: [
+      { name: 'Afghanistan', code: 'AF' },
+      { name: 'Åland Islands', code: 'AX' },
+      { name: 'Albania', code: 'AL' },
+      { name: 'Algeria', code: 'DZ' },
+      { name: 'American Samoa', code: 'AS' },
+      { name: 'Andorra', code: 'AD' },
+      { name: 'Angola', code: 'AO' },
+      { name: 'Anguilla', code: 'AI' },
+      { name: 'Antarctica', code: 'AQ' },
+      { name: 'Antigua and Barbuda', code: 'AG' },
+      { name: 'Argentina', code: 'AR' },
+      { name: 'Armenia', code: 'AM' },
+      { name: 'Aruba', code: 'AW' },
+      { name: 'Australia', code: 'AU' },
+      { name: 'Austria', code: 'AT' },
+      { name: 'Azerbaijan', code: 'AZ' },
+      { name: 'Bahamas', code: 'BS' },
+      { name: 'Bahrain', code: 'BH' },
+      { name: 'Bangladesh', code: 'BD' }
+    ]
+  });
