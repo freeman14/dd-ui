26117ad76ef7721cc8d8ab63add680da44c44057
Pavel <pkurochka@users.noreply.github.com>
2017-01-18 10:04:35 +0200

    - updated modals styles (#44)
    - minor fixes in styles
    - added an example for modals

--- src/modal/modal.module.ts
+++ src/modal/modal.module.ts
@@ -9,7 +9,22 @@ import run from './modal.run';
 const module: ng.IModule = angular.module('ep.modal', [
   'ngSanitize',
   'ui.bootstrap.modal'
-]);
+])
+  .component('demoModal', {
+    template: `<div>
+                <button ng-click="vm.openModal()" class="btn btn--primary">Open modal</button>
+              </div>`,
+    controllerAs: 'vm',
+    controller: function ($uibModal) {
+      'ngInject';
+
+      this.openModal = () => {
+        $uibModal.open({
+          template: '<div style="width: 300px; height: 200px; background: white"></div>',
+        });
+      };
+    }
+  });
 
 module.run(run);
 
