4a5b6c1a82193282dcdd1e00dd29767a558bde56
okolomiets <o.kolomiets@svitla.com>
2017-01-19 15:34:38 +0100

    Added Error pages (fixed style)

--- src/errorPage/error.run.ts
+++ src/errorPage/error.run.ts
@@ -2,45 +2,51 @@ export default function runFunc($templateCache: ng.ITemplateCacheService): void 
   'ngInject';
 
   $templateCache.put("./errorPage/error.partial.html",
-    `<header class="app-header error-page">
-      <div class="img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
-      <span class="header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
-    </header>
-    <div class="app-error-container">
-      <span class="app-error-icon dd-image-error" aria-hidden="true"></span>
-      <h1 class="app-error-title" ng-bind="vm.config.title"></h1>
-      <h5 class="app-error-message" ng-bind="vm.config.message"></h5>
-      <div class="app-error-footer">
-        <span class="app-error-link" ng-bind="vm.config.reloadLink" ng-click="vm.reload()"></span>
-        <span class="app-error-splitter"></span>
-        <span class="app-error-link" ng-bind="vm.config.supportLink" ng-click="vm.mailto(vm.config.supportLinkEmail, vm.config.supportEmailSubject)"></span>
+    `<div class="dd__error__page">
+      <header class="error-page-header">
+        <div class="error-page-img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
+        <span class="error-page-header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
+      </header>
+      <div class="error-page-container">
+        <span class="error-page-icon dd-image-error" aria-hidden="true"></span>
+        <h1 class="error-page-title" ng-bind="vm.config.title"></h1>
+        <h5 class="error-page-message" ng-bind="vm.config.message"></h5>
+        <div class="error-page-footer">
+          <span class="error-page-link" ng-bind="vm.config.reloadLink" ng-click="vm.reload()"></span>
+          <span class="error-page-splitter"></span>
+          <span class="error-page-link" ng-bind="vm.config.supportLink" ng-click="vm.mailto(vm.config.supportLinkEmail, vm.config.supportEmailSubject)"></span>
+        </div>
       </div>
     </div>`);
 
   $templateCache.put("./errorPage/maintenance.partial.html",
-    `<header class="app-header error-page">
-      <div class="img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
-      <span class="header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
-    </header>
-    <div class="app-error-container">
-      <span class="app-error-icon dd-image-maintenance" aria-hidden="true"></span>
-      <h1 class="app-error-title" ng-bind="vm.config.title"></h1>
-      <h5 class="app-error-message" ng-bind="vm.config.message"></h5>
+    `<div class="dd__error__page">
+      <header class="error-page-header">
+        <div class="error-page-img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
+        <span class="error-page-header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
+      </header>
+      <div class="error-page-container">
+        <span class="error-page-icon dd-image-maintenance" aria-hidden="true"></span>
+        <h1 class="error-page-title" ng-bind="vm.config.title"></h1>
+        <h5 class="error-page-message" ng-bind="vm.config.message"></h5>
+      </div>
     </div>`);
 
   $templateCache.put("./errorPage/notFound.partial.html",
-    `<header class="app-header error-page">
-      <div class="img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
-      <span class="header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
-    </header>
-    <div class="app-error-container">
-      <span class="app-error-icon dd-image-404" aria-hidden="true"></span>
-      <h1 class="app-error-title" ng-bind="vm.config.title"></h1>
-      <h5 class="app-error-message" ng-bind="vm.config.message"></h5>
-      <div class="app-error-footer">
-        <span class="app-error-link" ng-bind="vm.config.eventContentLink" ng-click="vm.goToLink(vm.config.eventContentLinkUrl)"></span>
-        <span class="app-error-splitter"></span>
-        <span class="app-error-link" ng-bind="vm.config.eventLayoutLink" ng-click="vm.goToLink(vm.config.eventLayoutLinkUrl)"></span>
-      </div>
+    `<div class="dd__error__page">
+      <header class="error-page-header">
+        <div class="error-page-img-logo dd-image-doubledutch" ng-click="vm.goToOrigin()"></div>
+        <span class="error-page-header-link" ng-bind="vm.config.helpLink" ng-click="vm.goToLink(vm.config.helpLinkUrl)"></span>
+      </header>
+      <div class="error-page-container">
+        <span class="error-page-icon dd-image-404" aria-hidden="true"></span>
+        <h1 class="error-page-title" ng-bind="vm.config.title"></h1>
+        <h5 class="error-page-message" ng-bind="vm.config.message"></h5>
+        <div class="error-page-footer">
+          <span class="error-page-link" ng-bind="vm.config.eventContentLink" ng-click="vm.goToLink(vm.config.eventContentLinkUrl)"></span>
+          <span class="error-page-splitter"></span>
+          <span class="error-page-link" ng-bind="vm.config.eventLayoutLink" ng-click="vm.goToLink(vm.config.eventLayoutLinkUrl)"></span>
+        </div>
+       </div>
     </div>`);
 };
