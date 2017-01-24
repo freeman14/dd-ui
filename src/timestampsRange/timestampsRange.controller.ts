0773bb59eead161324629d47e405187437dfdfd4
okolomiets <o.kolomiets@svitla.com>
2017-01-24 16:59:24 +0100

    Fixed doubled time in timestamp range with full date format provided

--- src/timestampsRange/timestampsRange.controller.ts
+++ src/timestampsRange/timestampsRange.controller.ts
@@ -14,6 +14,7 @@ export class TimestampsRangeController {
     dateFormat: string;
     startTimeFormat: string;
     endTimeFormat: string;
+    isTimeFormat: boolean;
     isSameDate: boolean;
 
   // @ngInject
@@ -25,15 +26,23 @@ export class TimestampsRangeController {
     }
 
     $onChanges(): void {
+      this.dateFormat = this.format || DATE_FORMAT;
       this.processDates(this.start, this.end, this.tz);
     }
 
     private processDates(start: string, end: string, tz: string): void {
       const startMoment: moment.Moment = momentTz.tz(start, tz);
       const endMoment: moment.Moment = momentTz.tz(end, tz);
-      this.isSameDate = startMoment.isSame(endMoment, 'day');
-      this.startTimeFormat = this.getTimeFormat(startMoment);
-      this.endTimeFormat = this.getTimeFormat(endMoment);
+
+      const matched = this.dateFormat.match(new RegExp('(,?\s*h:?m*a?)', 'i'));
+      this.isTimeFormat = matched && matched.length > 0;
+
+      if (this.isTimeFormat) {
+        this.dateFormat = this.dateFormat.replace(matched[0], '');
+        this.isSameDate = startMoment.isSame(endMoment, 'day');
+        this.startTimeFormat = this.getTimeFormat(startMoment);
+        this.endTimeFormat = this.getTimeFormat(endMoment);
+      }
     }
 
     private getTimeFormat(moment: moment.Moment): string {
