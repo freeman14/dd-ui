95aa72953b4f4d973081069e8ef1141c256edaa2
Oleg Kolomiets <okolomiets@gmail.com>
2017-01-25 10:37:39 +0100

    Timestamp range fix (#56)
    * Fixed doubled time in timestamp range with full date format provided
    
    * Fixed doubled time in timestamp range with full date format provided
    
    * Added logic to handle dates with the same month and year in timestamps range component

--- src/timestampsRange/timestampsRange.controller.ts
+++ src/timestampsRange/timestampsRange.controller.ts
@@ -2,6 +2,7 @@ import * as moment from 'moment';
 import * as momentTz from 'moment-timezone';
 
 const DATE_FORMAT: string = 'ddd, MMM D, YYYY';
+const MONTH_FORMAT: string = 'MMM D, YYYY';
 const TIME_FORMAT: string = 'h:mma';
 const SHORT_TIME_FORMAT: string = 'ha';
 
@@ -14,6 +15,8 @@ export class TimestampsRangeController {
     dateFormat: string;
     startTimeFormat: string;
     endTimeFormat: string;
+    isTimeFormat: boolean;
+    isMonthFormat: boolean;
     isSameDate: boolean;
 
   // @ngInject
@@ -25,15 +28,36 @@ export class TimestampsRangeController {
     }
 
     $onChanges(): void {
+      this.dateFormat = this.format || DATE_FORMAT;
       this.processDates(this.start, this.end, this.tz);
     }
 
     private processDates(start: string, end: string, tz: string): void {
       const startMoment: moment.Moment = momentTz.tz(start, tz);
       const endMoment: moment.Moment = momentTz.tz(end, tz);
+
+      const timeMatched = this.dateFormat.match(new RegExp('(,?\s*h:?m*a?)', 'i'));
+      this.isTimeFormat = timeMatched && timeMatched.length > 0;
+
+      this.isMonthFormat = this.dateFormat === MONTH_FORMAT;
       this.isSameDate = startMoment.isSame(endMoment, 'day');
-      this.startTimeFormat = this.getTimeFormat(startMoment);
-      this.endTimeFormat = this.getTimeFormat(endMoment);
+
+      if (this.isTimeFormat) {
+        this.dateFormat = this.dateFormat.replace(timeMatched[0], '');
+        this.startTimeFormat = this.getTimeFormat(startMoment);
+        this.endTimeFormat = this.getTimeFormat(endMoment);
+      } else {
+        this.startTimeFormat = this.endTimeFormat = this.dateFormat;
+      }
+
+      if (this.isMonthFormat) {
+        const isSameMonth = startMoment.isSame(endMoment, 'month');
+        const isSameYear = startMoment.isSame(endMoment, 'year');
+        if (isSameYear && isSameMonth) {
+          this.startTimeFormat = this.isSameDate ? MONTH_FORMAT : 'MMM D';
+          this.endTimeFormat = 'D, YYYY'
+        }
+      }
     }
 
     private getTimeFormat(moment: moment.Moment): string {
--- stories/jsComponents/timestamp/index.ts
+++ stories/jsComponents/timestamp/index.ts
@@ -5,13 +5,13 @@ UIComponentsSection.story('Timestamps')
   {
     date: new Date(),
     tz: 'America/Los_Angeles',
-    format: 'ddd, MMM D, YYYY'
+    format: 'ddd, MMM D, YYYY h:mma'
   })
   .add('Timestamp Range', '<timestamps-range start="vm.start" end="vm.end" tz="vm.tz" format="vm.format" class="demo__card"></timestamps-range>',
   {
     start: '2016-10-21T17:00:00.002Z',
     end: '2016-10-26T19:45:00.002Z',
     tz: 'America/Los_Angeles',
-    format: 'ddd, MMM D, YYYY'
+    format: 'ddd, MMM D, YYYY h:mma'
   })
   ;
