0a453d8929fd7144d0c847ad21c5cc01f292e34b
parxomchik <dmitriy.parkhomenko@icloud.com>
2017-02-14 11:34:24 +0200

    added disabled to checkboxes (#75)
    * added disabled to checkboxes
    
    * updated build

--- src/directives/checkbox/checkbox.directive.ts
+++ src/directives/checkbox/checkbox.directive.ts
@@ -1,38 +1,41 @@
 export default () => {
 
-    return {
-        restrict: 'E',
-        require: 'ngModel',
-        replace: true,
-        transclude: true,
-        template: require('./checkbox.partial.html'),
-        scope: {
-            ngModel: '=',
-            prechecked: '<?checkboxPrechecked',
-            activeColor: '<?color'
-        },
-        link: (scope: any, element: any, attrs: any, model: any) => {
-            scope.small = attrs.checkboxSmall !== undefined;
+  return {
+    restrict: 'E',
+    require: 'ngModel',
+    replace: true,
+    transclude: true,
+    template: require('./checkbox.partial.html'),
+    scope: {
+      ngModel: '=',
+      prechecked: '<?checkboxPrechecked',
+      activeColor: '<?color',
+      disabled: '<?disabled'
+    },
+    link: (scope: any, element: any, attrs: any, model: any) => {
+      scope.small = attrs.checkboxSmall !== undefined;
 
-            model.$formatters.unshift(value => {
-                scope.isChecked = value;
-                return value;
-            });
+      model.$formatters.unshift(value => {
+        scope.isChecked = value;
+        return value;
+      });
 
-            scope.getColors = (): any => {
-              if (scope.prechecked) {
-                return { 'border-color': scope.activeColor, color: scope.activeColor };
-              } else if (scope.isChecked) {
-                return { 'border-color': scope.activeColor, background: scope.activeColor };
-              }
-            };
-
-            scope.toggle = () => {
-                scope.isChecked = !scope.isChecked;
-                model.$setViewValue(scope.isChecked);
-            };
-
+      scope.getColors = (): any => {
+        if (scope.prechecked) {
+          return { 'border-color': scope.activeColor, color: scope.activeColor };
+        } else if (scope.isChecked) {
+          return { 'border-color': scope.activeColor, background: scope.activeColor };
         }
-    };
+      };
+
+      scope.toggle = () => {
+        if (!scope.disabled) {
+          scope.isChecked = !scope.isChecked;
+          model.$setViewValue(scope.isChecked);
+        }
+      };
+
+    }
+  };
 
 };
