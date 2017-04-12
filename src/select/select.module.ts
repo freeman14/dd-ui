558e99a25f672e21273af017f6a1e5b5cb4a201b
pkurochka <p.kurochka@svitla.com>
2017-04-12 10:32:48 +0300

    - added directive to fix select focus logic

--- src/select/select.module.ts
+++ src/select/select.module.ts
@@ -1,6 +1,7 @@
 import * as angular from 'angular';
 
 import 'ui-select';
+import { disableAnimate } from './disable-animate.directive';
 
 const module: ng.IModule = angular.module('ep', [
     'ui.select'
@@ -10,5 +11,6 @@ module.config(function (uiSelectConfig: any): void {
     uiSelectConfig.theme = 'selectize';
     uiSelectConfig.removeSelected = false;
 });
+module.directive('disableAnimate', disableAnimate);
 
 export default module.name;
