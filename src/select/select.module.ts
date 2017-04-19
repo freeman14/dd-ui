296f56cd5e5e423c5ff53771100fb2325f1fcd17
Pavel <pkurochka@users.noreply.github.com>
2017-04-19 14:47:34 +0300

    STEW-1106, STEW-1107 (#106)
    * select highlight fixes
    
    * select single preview

--- src/select/select.module.ts
+++ src/select/select.module.ts
@@ -2,6 +2,7 @@ import * as angular from 'angular';
 
 import 'ui-select';
 import { disableAnimate } from './disable-animate.directive';
+import run from './select.run';
 
 const module: ng.IModule = angular.module('ep', [
     'ui.select'
@@ -11,6 +12,7 @@ module.config(function (uiSelectConfig: any): void {
     uiSelectConfig.theme = 'selectize';
     uiSelectConfig.removeSelected = false;
 });
+module.run(run);
 module.directive('disableAnimate', disableAnimate);
 
 export default module.name;
