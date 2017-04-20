6fdb7a5cb85b450fac3744badd0adf104a9163ca
Mykhailo Churilov <m.churilov@svitla.com>
2017-04-20 14:04:53 +0300

    updated demo input

--- stories/directives/inputLimit.ts
+++ stories/directives/inputLimit.ts
@@ -1,7 +1,7 @@
 let section: stotybook.ISection = sb.section('Directives');
 
 section.story('Text Limit')
-    .add('Input limit', `<div class="dd__input"><input type="text" text-limit="100" ng-model="vm.name"></div>`, { name: 'John Doe' })
+    .add('Input limit', `<div class="dd__input"><input type="text" text-limit="100" visible-limit ng-model="vm.name"></div>`, { name: 'John Doe' })
     .add('Textarea limit',
     ` <textarea ng-model="vm.name" text-limit="4000"></textarea>`,
     { name: 'John Doe' });
