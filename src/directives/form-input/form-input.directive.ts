47c3edf2ec95babfc8ec5a1d6293b5d0ac6cccba
pkurochka <p.kurochka@svitla.com>
2017-05-11 14:55:57 +0300

    added resetting form submitted state on input focus

--- src/directives/form-input/form-input.directive.ts
+++ src/directives/form-input/form-input.directive.ts
@@ -1,11 +1,15 @@
-export function formInput (): ng.IDirective {
+export function formInput ($timeout): ng.IDirective {
   'ngInject';
 
   return {
-    require: 'ngModel',
-    link: (scope, iElement, iAttrs, ngModelController: ng.INgModelController) => {
-      iElement.on('focus', () => scope.$apply(() => ngModelController.$setPristine()));
-      iElement.on('blur', () => scope.$apply(() => ngModelController.$setDirty()));
+    require: ['ngModel', '^form'],
+    link: (scope, iElement, iAttrs, ctrls: any[]) => {
+      const [ngModelController, form] = ctrls;
+      iElement.on('focus', () => $timeout(() => {
+        ngModelController.$setPristine();
+        form.$ddSubmitted = false;
+      }, 0));
+      iElement.on('blur', () => $timeout(() => ngModelController.$setDirty(), 0));
       ngModelController['$options'] = { updateOn: 'blur' };
 
       scope.$on('$destroy', () => {
